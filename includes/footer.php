1741808de2c4cfe78480e773d3a6e207992b4bc7
JetmireSyla <js40336@ubt-uni.net>
2021-02-10 23:00:02 +0100

    header, footer dhe admin

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+    <div class="footer">
+        <div class="container">
+            <div>
+                <h3>Call: (+383) 44 111 222</h3>
+            </div>
+            <div class="icon-div">
+                <p class="paragraph-design2">
+                    <i style="color: #ffffff;" class="fas fa-map-marker-alt fa-1x">
+                    </i> Magj. Prishtinë-Ferizaj</p>
+            </div>
+            <div class="icon-div">
+                <p class="paragraph-design2">
+                    <i style="color: #ffffff;" class="fas fa-envelope  fa-1x"></i> yjet_info@autosalon.com
+                </p>
+            </div>
+            <div class="icon-div">
+                <p class="paragraph-design2">
+                    <i style="color: #ffffff;" class="fas fa-clock fa-1x"></i> Mon-Fri: 9am-6pm | Sat: 9am-1pm
+                </p>
+            </div>
+
+            <div class="copyright">
+                <p style="color: #ffffff"><i>Copyright © 2021 Autosalloni YJET. All rights reserved</i></p>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
