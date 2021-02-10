0076299048e703296bbda6c9fd337a60f56d8796
JetmireSyla <63473908+JetmireSyla@users.noreply.github.com>
2021-02-10 23:21:37 +0100

    header, footer dhe admin-sidemenu

---
+++ includes/admin-sidemenu.php
@@ -0,0 +1,74 @@
+<?php 
+    session_start();
+    if($_SESSION['role'] != 'Admin'){
+        header("Location: index.php");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>AUTOSALLONI YJET | 
+    	<?php 
+
+        if($page == 'admin'){
+            echo 'User';
+        }else if($page == 'messages'){
+            echo 'Messages';
+        }else if($page == 'admin-cars'){
+            echo 'Products';
+        }else if($page == 'add-cars'){
+            echo 'Add';
+        }else if($page == 'edit-cars'){
+            echo 'Edit';
+        }
+
+
+    ?> 
+    	
+    </title>
+    <meta charset="utf-8" >
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width , initial-scale=1">   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <script type="text/javascript" src="/js/script.js"></script>
+
+</head>
+<body>
+
+ <header>
+        <div class="admin-header">
+                <a href="index.php"><div class="admin-logo">
+                    <h3>AUTOSALLONI YJET</h3>
+                </div></a>
+        </div>
+    </header> 
+
+    <div class="side-nav">
+        <nav>
+            <ul>
+                <li>
+                    <a href="admin.php">
+                        <p><i class="fa fa-user" style="margin-right: 5px;"></i> Users</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="messages.php">
+                        <p><i class="fa fa-envelope" style="margin-right: 5px;"></i> Messages</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="admin-cars.php">
+                        <p><i class="fa fa-car" aria-hidden="true" style="margin-right: 5px;"></i> Cars</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="logout.php">
+                        <p>Log out</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div> 
---
+++ includes/header.php
@@ -0,0 +1,84 @@
+<?php  
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>AUTOSALLONI YJET | 
+    <?php 
+
+        if($page == 'index'){
+            echo 'Home';
+        }else if($page == 'cars'){
+            echo 'Products';
+        }else if($page == 'about'){
+            echo 'About';
+        }else if($page == 'contact'){
+            echo 'Contact';
+        }else if($page == 'login'){
+            echo 'Login';
+        if(isset($_SESSION['email'])){
+            header("Location: index.php");
+        }
+        }else if($page == 'register'){
+            echo 'Register';
+            if(isset($_SESSION['email'])){
+                header("Location: index.php");
+            }
+        }else if($page == 'cardetails'){
+            echo 'Car Details';
+        }
+
+
+    ?> 
+    </title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script> 
+    <script type="text/javascript" src="js/script.js"></script>
+
+</head>
+
+<body>
+    <header>
+        <div class="topnav">
+            <div class="container">
+                <div class="logo">
+                    <h3>AutoSalloni YJET</h3>
+                </div>
+                <div class="links">
+                <?php
+                        if(isset($_SESSION['role'])){
+                            if( $_SESSION['role'] == "Admin"){
+
+                            
+
+                    ?>
+                    <a href="admin.php">Admin Panel</a>
+
+                        <?php }} ?>
+                    <a class="<?php if($page == 'index'){echo 'active';}?>" href="index.php">Home</a>
+                    <a class="<?php if($page == 'cars'){echo 'active';}?>" href="cars.php">Products</a>
+                    <a class="<?php if($page == 'about'){echo 'active';}?>" href="about.php">About</a>
+                    <a class="<?php if($page == 'contact'){echo 'active';}?>" href="contact.php">Contact</a>
+                    <?php
+                        if(isset($_SESSION['email'])){
+
+                    ?>
+                    <button class="login-design" onclick="location.href='logout.php'" type="button">
+                        Logout</button>
+
+                        <?php }else{ ?>
+                    <button class="login-design" onclick="location.href='login.php'" type="button">
+                        Login</button>
+                        <?php } ?>
+                </div>
+            </div>
+        </div>
+    </header>
