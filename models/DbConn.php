74e11a8b880120e3ba281ad0b436d9d94573f12e
JetmireSyla <63473908+JetmireSyla@users.noreply.github.com>
2021-03-15 17:55:08 +0100

    Update DbConn.php

--- models/DbConn.php
+++ models/DbConn.php
@@ -4,7 +4,7 @@ class DBconnection{
 	public function getConnection(){
 
 		try{
-			$connection = new PDO("mysql:host=localhost;dbname=autosalon_yjet", "root" , "");   
+			$connection = new PDO("mysql:host=localhost;dbname=autosalloni_yjet", "root" , "");   
             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch(PDOException $e){
 			echo $e->getMessage();
